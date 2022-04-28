705cb46449e2d1319efbb3582d08fde96d8bd0e4
persephone-d <104580200+persephone-d@users.noreply.github.com>
2022-04-28 16:02:54 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("EMILY")
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("E B C5 A B G A F ", 40)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C5 B A G F E D C ", 40)
+})
+basic.forever(function () {
+	
+})
